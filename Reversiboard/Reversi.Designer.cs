3d9787405ccdddb2863bd7e2ff9ffd6f41b44ea7
JustNireon <siemvdtweel@gmail.com>
2018-10-12 12:42:38 +0200

    add project to git

---
+++ Reversiboard/Reversi.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Reversiboard
+{
+    partial class Reversi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlReversi = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.25F));
+            this.tableLayoutPanel1.Controls.Add(this.pnlReversi, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pnlReversi
+            // 
+            this.pnlReversi.BackColor = System.Drawing.Color.White;
+            this.pnlReversi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlReversi.Location = new System.Drawing.Point(302, 0);
+            this.pnlReversi.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlReversi.Name = "pnlReversi";
+            this.pnlReversi.Size = new System.Drawing.Size(498, 450);
+            this.pnlReversi.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 450);
+            this.panel2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 2;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(80, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 29);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Help";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(75, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wit heeft {} stenen";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(79, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Pas";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(79, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Nieuw spel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Wit is aan zet.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(75, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zwart heeft {} stenen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 21.75F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(72, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Reversi™";
+            // 
+            // Reversi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Reversi";
+            this.Text = "Form2";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlReversi;
+    }
+}
---
+++ Reversiboard/Reversi.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversiboard
+{
+    public partial class Reversi : Form
+    {
+        public Reversi()
+        {
+            InitializeComponent();
+            reversiboard rb = new reversiboard(pnlReversi);
+            pnlReversi.Paint += rb.rbRender;
+            pnlReversi.MouseClick += rb.playTurn;
+        }
+    }
+    public class reversiboard
+    {
+        disk[,] diskarray;
+        Panel Display;
+        int totalturn;
+        int currentturn;
+        public reversiboard(Panel pnlDisp)
+        {
+            Display = pnlDisp;
+            diskarray = new disk[8,8];
+            startgame();
+        }
+        private void startgame()
+        {
+            for (int i = 0; i < diskarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < diskarray.GetLength(1); j++)
+                {
+                    diskarray[i, j] = new disk();
+                }
+            }
+            diskarray[diskarray.GetLength(0) / 2, diskarray.GetLength(1) / 2].state=0;
+            diskarray[diskarray.GetLength(0) / 2 - 1, diskarray.GetLength(1) / 2 - 1].state = 0;
+            diskarray[diskarray.GetLength(0) / 2-1, diskarray.GetLength(1) / 2].state = 1;
+            diskarray[diskarray.GetLength(0) / 2, diskarray.GetLength(1) / 2 - 1].state = 1;
+            totalturn = diskarray.GetLength(0) * diskarray.GetLength(1);
+            currentturn = 0;
+        }
+        private void SetState(int x,int y,int state)
+        {
+            diskarray[x,y].state = state;
+        }
+
+        public void rbRender (object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < diskarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < diskarray.GetLength(1); j++)
+                {
+                    if (diskarray[i, j].state == -2)
+                    {
+                        diskarray[i, j].state = -1;
+                    }
+                }
+            }
+            for (int i = 0; i < diskarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < diskarray.GetLength(1); j++)
+                {
+                    if (diskarray[i, j].state == currentturn%2)
+                    {
+                        GameCheck(i, j, currentturn%2,true);
+                    }
+                }
+            }
+            Color diskcolor = Color.Green;
+            int panelWidth = Display.Width / diskarray.GetLength(0);
+            int panelHeight = Display.Height / diskarray.GetLength(1);
+
+            for (int i = 0; i < diskarray.GetLength(0); i++){
+                for(int j = 0; j < diskarray.GetLength(1); j++)
+                {
+                    switch (diskarray[i, j].state)
+                    {
+                        case -2: diskcolor = Color.Blue; break;
+                        case -1: diskcolor = Color.Green; break;
+                        case 0: diskcolor = Color.White; break;
+                        case 1: diskcolor = Color.Black; break;
+                    }
+                    e.Graphics.FillRectangle(new SolidBrush(diskcolor), new Rectangle(new Point(i * panelWidth,j * panelHeight), new Size(panelWidth, panelHeight)));
+                }
+            }
+            for (int i = 0; i < diskarray.GetLength(0); i++) { e.Graphics.DrawLine(new Pen(Brushes.Gray, 3), new Point(i * panelWidth, 0), new Point(i * panelWidth, Display.Height)); }
+            for (int i = 0; i < diskarray.GetLength(1); i++) { e.Graphics.DrawLine(new Pen(Brushes.Gray, 3), new Point(0, i * panelHeight), new Point(Display.Width, i * panelHeight)); }
+        }
+        private void GameCheck (int X, int Y, int user,bool spec)
+        {
+            for(int i = -1; i < 2 && X+i >= 0 && X+i < diskarray.GetLength(0); i++)
+            {
+                for (int j = -1; j <2 && Y + j >= 0 && Y + j < diskarray.GetLength(1); j++)
+                {
+
+                    if (i == 0 && j == 0) { continue; }
+                    if (diskarray[X + i, Y + j].state > -1 && diskarray[X + i, Y + j].state != user)
+                    {
+                        extendedCheck(X, Y, user, i, j, spec);
+                    }
+                }
+            }
+        }
+
+        private void extendedCheck(int X, int Y, int User, int dirX, int dirY,bool spec)
+        {
+            for(int i = 1; X+i*dirX >= 0 && X+i*dirX < diskarray.GetLength(0)&& Y+i*dirY >= 0 && Y+i*dirY < diskarray.GetLength(1); i++)
+            {
+
+                if (diskarray[X + i*dirX, Y + i*dirY].state < 0) { if (spec) { diskarray[X + i * dirX, Y + i * dirY].state = -2; break; } else { break; } }
+                else if (diskarray[X + i*dirX, Y + i*dirY].state == User&& !spec)
+                {
+                    
+                    for (int j = i; j > 0; j--)
+                    {
+                        diskarray[X+j*dirX, Y+j*dirY].state = User;
+
+                    }
+                    break;
+                }
+            }
+        }      
+        public void playTurn(object sender, MouseEventArgs mea)
+        {
+            Point clickpoint = new Point((int)Math.Floor((float)mea.X / (Display.Width/diskarray.GetLength(0))), (int)Math.Floor((float)mea.Y / (Display.Height/diskarray.GetLength(1))));
+            if (diskarray[clickpoint.X,clickpoint.Y].state == -2)
+            { 
+                diskarray[clickpoint.X, clickpoint.Y].state = currentturn % 2;
+                this.GameCheck(clickpoint.X,clickpoint.Y, currentturn %2,false);
+                currentturn++;
+                Display.Invalidate();
+            }
+            if (currentturn == totalturn)
+            {
+                int whitesum = 0;
+                int blacksum = 0;
+                for (int i = 0; i < diskarray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < diskarray.GetLength(1); j++)
+                    {
+                        if (diskarray[i,j].state == 0)
+                        {
+                            whitesum++;
+                        }
+                        else
+                        {
+                            blacksum++;
+                        }
+                    }
+                }
+                if (whitesum > blacksum) {
+                    MessageBox.Show("White won the game");
+                }
+                else if(whitesum == blacksum)
+                {
+                    MessageBox.Show("It's a Draw");
+                }
+                else
+                {
+                    MessageBox.Show("Black won the Game");
+                }
+            }
+        }
+
+        private class disk
+        {
+            public int state;
+
+            // -2 Je kan hier plaatsen
+            // -1 Beschikbaar
+            // 0  Wit 
+            // 1 Zwart
+            public disk()
+            {
+                state = -1;
+            }
+        }
+    }
+}
+
